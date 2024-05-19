d0d7317ed5538b0d72591656b0db4b79be59ea2a
c117031 <raphael.cs@hotmail.com>
2024-05-19 18:35:16 -0300

    conta investimento pj funcionando

---
+++ src/main/java/tech/ada/banco/controller/ContaInvestimentoPJController.java
@@ -0,0 +1,97 @@
+package tech.ada.banco.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import tech.ada.banco.dto.ContaDTO;
+import tech.ada.banco.exception.NaoEncontradoException;
+import tech.ada.banco.exception.SaldoExistenteException;
+import tech.ada.banco.exception.SaldoInsuficienteException;
+import tech.ada.banco.exception.ValorInvalidoException;
+import tech.ada.banco.login.JwtService;
+import tech.ada.banco.service.ContaInvestimentoPFService;
+import tech.ada.banco.service.ContaInvestimentoPJService;
+import tech.ada.banco.usuario.Usuario;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/clientesPJ/contaInvestimento")
+@RequiredArgsConstructor
+public class ContaInvestimentoPJController {
+
+    private final ContaInvestimentoPJService service;
+    private final ModelMapper modelMapper;
+    private final JwtService jwtService;
+
+    @GetMapping("/{uuid}")
+    public List<ContaDTO> listarContasUsuario(@PathVariable("uuid") UUID uuid){
+        return service.listarContas(uuid).stream().collect(Collectors.toList());
+    }
+
+    @PostMapping
+    @PreAuthorize("hasAnyRole(T(tech.ada.banco.usuario.Role).ADMIN.name(),T(tech.ada.banco.usuario.Role).FUNCIONARIO.name())")
+    public ResponseEntity<ContaDTO> inserir(@Valid @RequestBody ContaDTO contaDTO){
+        try {
+            return new ResponseEntity<>(service.salvar(contaDTO), HttpStatus.CREATED);
+        }catch (NaoEncontradoException e){
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @DeleteMapping("/{uuid}")
+    @PreAuthorize("hasAnyRole(T(tech.ada.banco.usuario.Role).ADMIN.name(),T(tech.ada.banco.usuario.Role).FUNCIONARIO.name())")
+    public ResponseEntity excluir(@PathVariable("uuid") UUID uuid){
+        try {
+            service.excluir(uuid);
+            return ResponseEntity.noContent().build();
+        } catch (SaldoExistenteException e) {
+            return new ResponseEntity(e.getMessage(),HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("/saldo/{uuid}")
+    @PreAuthorize("hasAnyRole(T(tech.ada.banco.usuario.Role).ADMIN.name(),T(tech.ada.banco.usuario.Role).FUNCIONARIO.name(),T(tech.ada.banco.usuario.Role).CLIENTE.name())")
+    public ResponseEntity consultarSaldo(@PathVariable("uuid") UUID uuid){
+        return new ResponseEntity<>(service.consutarSaldo(uuid), HttpStatus.OK);
+    }
+
+    @PostMapping("/sacar")
+    @PreAuthorize("hasAnyRole(T(tech.ada.banco.usuario.Role).ADMIN.name(),T(tech.ada.banco.usuario.Role).CLIENTE.name())")
+    public ResponseEntity<ContaDTO> sacar(@Valid @RequestBody ContaDTO contaDTO, @RequestHeader (name="Authorization") String bearerToken){
+        try {
+          //  service.validarUsuario(obterCpfToken(bearerToken),contaDTO);
+            return new ResponseEntity<>(service.sacar(contaDTO), HttpStatus.OK);
+        } catch (SaldoInsuficienteException e) {
+            return new ResponseEntity(e.getMessage(),HttpStatus.BAD_REQUEST);
+        } catch (ValorInvalidoException e) {
+            return new ResponseEntity(e.getMessage(),HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PostMapping("/investir")
+    @PreAuthorize("hasAnyRole(T(tech.ada.banco.usuario.Role).ADMIN.name(),T(tech.ada.banco.usuario.Role).CLIENTE.name())")
+    public ResponseEntity<ContaDTO> investir(@Valid @RequestBody ContaDTO contaDTO, @RequestHeader (name="Authorization") String bearerToken){
+        try {
+           // service.validarUsuario(obterCpfToken(bearerToken),contaDTO);
+            return new ResponseEntity<>(service.investir(contaDTO), HttpStatus.OK);
+        } catch (SaldoInsuficienteException e) {
+            return new ResponseEntity(e.getMessage(),HttpStatus.BAD_REQUEST);
+        } catch (ValorInvalidoException e) {
+            return new ResponseEntity(e.getMessage(),HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    private String obterCpfToken(String bearerToken){
+        String token = bearerToken.substring(7);
+        Usuario u = (Usuario) jwtService.getUserDetails(token);
+        return u.getCpf();
+    }
+
+}
--- src/main/java/tech/ada/banco/service/ContaInvestimentoPJService.java
+++ src/main/java/tech/ada/banco/service/ContaInvestimentoPJService.java
@@ -1,12 +1,93 @@
 package tech.ada.banco.service;
 
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.stereotype.Service;
+import tech.ada.banco.dto.ContaDTO;
+import tech.ada.banco.exception.NaoEncontradoException;
+import tech.ada.banco.exception.SaldoExistenteException;
+import tech.ada.banco.exception.SaldoInsuficienteException;
+import tech.ada.banco.exception.ValorInvalidoException;
+import tech.ada.banco.model.ClientePF;
+import tech.ada.banco.model.ClientePJ;
 import tech.ada.banco.model.ContaInvestimento;
+import tech.ada.banco.repository.ClienteRepository;
+import tech.ada.banco.repository.ContaRepository;
 import tech.ada.banco.service.consultaSaldo.ConsultaSaldo;
 import tech.ada.banco.service.deposito.Deposito;
 import tech.ada.banco.service.investimento.InvestirPJImpl;
 import tech.ada.banco.service.saque.SaquePJImpl;
 import tech.ada.banco.service.transferencia.TransferenciaPJImpl;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
 public class ContaInvestimentoPJService implements SaquePJImpl<ContaInvestimento>, ConsultaSaldo<ContaInvestimento>,
-        Deposito<ContaInvestimento>, TransferenciaPJImpl<ContaInvestimento>, InvestirPJImpl {
+        InvestirPJImpl {
+
+    private final ContaRepository<ClientePJ, ContaInvestimento> contaRepository;
+    private final ClienteRepository<ClientePJ> clienteRepository;
+    private final ModelMapper modelMapper;
+
+    private ContaDTO convertDto(ContaInvestimento conta){
+        return modelMapper.map(conta, ContaDTO.class);
+    }
+
+    private ContaInvestimento convertFromDto(ContaDTO contaDTO){
+        return modelMapper.map(contaDTO, ContaInvestimento.class);
+    }
+
+    public List<ContaDTO> listarContas(UUID clienteUuid){
+        var cliente = clienteRepository.findByUuid(clienteUuid);
+        return contaRepository.findByCliente(cliente).stream().map(this::convertDto).collect(Collectors.toList());
+    }
+
+    public ContaDTO salvar(ContaDTO contaDTO) throws NaoEncontradoException {
+        var cliente = clienteRepository.findByUuid(contaDTO.getClienteUuid()).orElseThrow();
+        var conta = convertFromDto(contaDTO);
+        conta.setCliente(cliente);
+        conta.setUuid(UUID.randomUUID());
+        conta.setDataCriacao(LocalDate.now());
+        return convertDto(contaRepository.save(conta));
+    }
+
+    public void excluir(UUID uuid) throws SaldoExistenteException {
+        var conta = contaRepository.findByUuid(uuid).orElseThrow();
+        if(conta.getSaldo().compareTo(BigDecimal.ZERO)==0){
+            contaRepository.delete(conta);
+        }else{
+            throw new SaldoExistenteException();
+        }
+    }
+
+    public ContaDTO sacar(ContaDTO contaDTO) throws SaldoInsuficienteException, ValorInvalidoException {
+        var conta = contaRepository.findByUuid(contaDTO.getUuid()).orElseThrow();
+        sacar((ClientePJ) conta.getCliente(), (ContaInvestimento) conta, contaDTO.getValorOperacao());
+        return convertDto(contaRepository.save(conta));
+    }
+
+    public ContaDTO consutarSaldo(UUID uuid)  {
+        var conta = contaRepository.findByUuid(uuid).orElseThrow();
+        return convertDto(conta);
+    }
+
+    public ContaDTO investir(ContaDTO contaDTO) throws SaldoInsuficienteException, ValorInvalidoException {
+        var conta = contaRepository.findByUuid(contaDTO.getUuid()).orElseThrow();
+        investir((ClientePJ) conta.getCliente(),(ContaInvestimento) conta,contaDTO.getValorOperacao());
+        contaRepository.save(conta);
+        return convertDto(conta);
+    }
+
+    public void validarUsuario(String cpf, ContaDTO contaDTO){
+        var conta = contaRepository.findByUuid(contaDTO.getUuid()).orElseThrow();
+        if(!((ClientePF)conta.getCliente()).getCpf().equals(cpf)) {
+            throw new AccessDeniedException("Apenas o dono da conta pode realizar esta operação.");
+        }
+    }
 }
